6391d089d386a394fe1e793166e26ef89ac49158
Mohammad Vahabian <mohamad.vahabian@gmail.com>
2022-07-25 12:25:42 +0430

    upload image and file

---
+++ store/validator.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+
+
+def file_size_validator(file):
+    max_file_size = 50
+
+    if file.size >max_file_size:
+        raise ValidationError(f'file size is more than {max_file_size} KB!')
