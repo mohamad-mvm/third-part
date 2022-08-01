9d10c2144d7a3cb9e7853747c739a9be79530377
Mohammad Vahabian <mohamad.vahabian@gmail.com>
2022-08-01 23:45:28 +0430

    email sending

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,31 @@
+import contextlib
+from django.core.mail import EmailMessage ,BadHeaderError,mail_admins
+from django.http import HttpResponse
+from templated_mail.mail import BaseEmailMessage
 from django.shortcuts import render
 
 
 def say_hello(request):
+    # try:
+    #     mail_admins('Subject', 'Body',html_message='<p>This is a test</p>')
+    # except BadHeaderError:
+    #     pass
+
+    # try:
+    #     message = EmailMessage('Subject', 'Body','mohamad@mvm.com' ,to=['ali.tt.com'])
+    #     message.attach_file('playground/static/images/Capture.JPG')
+    #     message.send()
+    # except BadHeaderError:
+    #     pass
+
+
+    try:
+        message = BaseEmailMessage(
+            template_name='playground/emails/temp.html',
+            context={'name': 'Mohamad'},
+        )
+        message.send(['ali.tt.com'])
+    except BadHeaderError:
+        pass
+
     return render(request, 'hello.html', {'name': 'Mosh'})
